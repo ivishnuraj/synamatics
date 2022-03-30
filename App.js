def603f827903d318a85f31be501ba7ab1702063
Vishnu Raj <synamatics@gmail.com>
2022-03-30 08:16:50 +0530

    initial

--- App.js
+++ App.js
@@ -1,15 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Alert, StyleSheet } from 'react-native'
+import React, { Component } from 'react'
+import * as Updates from 'expo-updates';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export class App extends Component {
+  async checkForUpdates() {
+      Alert.alert("Checking for updates")
+        try {
+      const update = await Updates.checkForUpdateAsync();
+      if (update.isAvailable) {
+        Alert.alert("New Update Available")
+        // await Updates.fetchUpdateAsync();
+        // // ... notify user of update ...
+        // Updates.reloadAsync();
+      }
+      else{
+        Alert.alert("Update Not Available")
+      }
+    } catch (e) {
+        Alert.alert(e.message)
+      }    
+  }
+  componentDidMount() {
+    this.checkForUpdates();
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>App</Text>
+      </View>
+    )
+  }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -18,3 +39,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App
