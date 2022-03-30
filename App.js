8fa6f5cf68098f423ee5e6cd9fcd994c63ef66d8
Vishnu Raj <synamatics@gmail.com>
2022-03-30 10:15:57 +0530

    expo

--- App.js
+++ App.js
@@ -1,85 +1,61 @@
-import React from 'react';
-import { Button, StyleSheet, View } from 'react-native';
-import LottieView from 'lottie-react-native';
+import { Text, View, Alert, StyleSheet } from 'react-native'
+import React, { Component } from 'react'
 import * as Updates from 'expo-updates';
 
-export default class App extends React.Component {
-  componentDidMount() {
-    this.animation.play();
-    this.checkForUpdates();
-    // Or set a specific startFrame and endFrame with:
-    // this.animation.play(30, 120);
-  }
+export class App extends Component {
+  
   async checkForUpdates() {
-    try {
-  const update = await Updates.checkForUpdateAsync();
-  if (update.isAvailable) {
-    Alert.alert('Update Available', `Version ${update.manifest.version} is available to download. Download it to expierience`, [
-      {
-        text: 'Download',
-        onPress: async () => {
-          let updates = await Updates.fetchUpdateAsync();
-          if(updates.isNew) {
-            Alert.alert("Update Downloaded","Relaunch App",[
-              {
-                text: 'Relaunch',
-                onPress: () => Updates.reloadAsync(),
-              },
-              {
-                text: 'Cancel',
-                onPress: () => {},
-                style: 'cancel',
-              },
-            ])
-          }
-        },
-      },
-      { text: 'Cancel', onPress: () => console.log('OK Pressed'),style: 'cancel', },
-    ]);
+        try {
+      const update = await Updates.checkForUpdateAsync();
+      if (update.isAvailable) {
+        Alert.alert('Update Available', `Version ${update.manifest.version} is available to download. Download it to expierience`, [
+          {
+            text: 'Download',
+            onPress: async () => {
+              let updates = await Updates.fetchUpdateAsync();
+              if(updates.isNew) {
+                Alert.alert("Update Downloaded","Relaunch App",[
+                  {
+                    text: 'Relaunch',
+                    onPress: () => Updates.reloadAsync(),
+                  },
+                  {
+                    text: 'Cancel',
+                    onPress: () => {},
+                    style: 'cancel',
+                  },
+                ])
+              }
+            },
+          },
+          { text: 'Cancel', onPress: () => console.log('OK Pressed'),style: 'cancel', },
+        ]);
+      }
+      else{
+        
+      }
+    } catch (e) {
+        Alert.alert(e.message)
+      }    
   }
-  else{
-    
+  componentDidMount() {
+    this.checkForUpdates();
   }
-} catch (e) {
-    Alert.alert(e.message)
-  }    
-}
-  resetAnimation = () => {
-    this.animation.reset();
-    this.animation.play();
-  };
-
   render() {
     return (
-      <View style={styles.animationContainer}>
-        <LottieView
-          ref={animation => {
-            this.animation = animation;
-          }}
-          style={{
-            width: 400,
-            height: 400,
-          }}
-          source={require('./assets/lf20_gehghjgo.json')}
-          // OR find more Lottie files @ https://lottiefiles.com/featured
-          // Just click the one you like, place that file in the 'assets' folder to the left, and replace the above 'require' statement
-        />
-        <View style={styles.buttonContainer}>
-          <Button title="Restart Animation" onPress={this.resetAnimation} />
-        </View>
+      <View style={styles.container}>
+        <Text>Update Checker</Text>
       </View>
-    );
+    )
   }
 }
-
 const styles = StyleSheet.create({
-  animationContainer: {
+  container: {
+    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    flex: 1,
-  },
-  buttonContainer: {
-    paddingTop: 20,
   },
 });
+
+export default App
